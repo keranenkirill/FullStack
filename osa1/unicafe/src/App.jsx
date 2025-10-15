e65b60b11a1d36511ba21a99e6cac43f9c5c1248
Kirill Keränen <keranen.kirill@gmail.com>
2025-10-15 09:16:37 +0300

    T1.6: unicafe step1

---
+++ osa1/unicafe/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+
+const Display = props => (
+  <div>
+    {props.text} {props.val}
+  </div>
+)
+const Button = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
+)
+
+
+const App = () => {
+  const [goodval, setGValue] = useState(0)
+  const [neutval, setNValue] = useState(0)
+  const [badval, setBValue] = useState(0)
+
+  const setGoodValue = newValue => {
+    console.log('good value now', newValue)
+    setGValue(newValue)
+  }
+
+  const setNeutValue = newValue => {
+    console.log('neut value now', newValue)
+    setNValue(newValue)
+  }
+  const setBadValue = newValue => {
+    console.log('bad value now', newValue)
+    setBValue(newValue)
+  }
+
+  return (
+    <div>
+      <p>Give Feedback</p>
+      <Button onClick={() => setGoodValue(goodval + 1)} text="G" />
+      <Button onClick={() => setNeutValue(neutval + 1)} text="N" />
+      <Button onClick={() => setBadValue(badval + 1)} text="B" />
+      <p>Statistics:</p>
+      <Display text="Good" val={goodval} />
+      <Display text="Neutral" val={neutval} />
+      <Display text="Bad" val={badval} />
+    </div>
+  )
+}
+
+export default App
