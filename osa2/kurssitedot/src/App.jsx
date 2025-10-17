f4a373481415be98858af5d1249e93e742fdc5cb
Kirill Keränen <keranen.kirill@gmail.com>
2025-10-17 14:19:05 +0300

    2.1: kurssitiedot step6

---
+++ osa2/kurssitedot/src/App.jsx
@@ -0,0 +1,83 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+const Course = (props) => {
+  return(
+    <div>
+      <Header name={props.course.name}/>
+      <Content parts={props.course.parts}/>
+    </div>
+  )
+
+}
+
+const Header = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>{props.name}</h1>
+    </div>
+  )
+}
+
+
+const Content = (props) => {
+  console.log(props)
+  const total = props.parts.reduce((sum, part) => sum + part.exercises, 0);
+
+  return (
+    <div>
+      {props.parts.map(part => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+      <Total total={total} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>{props.name} {props.exercises}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Total sum of submitted exercises is {props.total}</p>
+    </div>
+  )
+}
+
+export default App
