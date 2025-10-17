4b5d7ee741a1089ac199a5cd69dcdfebae546baf
Kirill Keränen <keranen.kirill@gmail.com>
2025-10-17 14:58:04 +0300

    2.5: kurssitedot - erillinen moduuli

--- osa2/kurssitedot/src/App.jsx
+++ osa2/kurssitedot/src/App.jsx
@@ -1,49 +1,6 @@
-const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+import courses from './components/Course'
 
+const App = () => {
   return (
     <div>
       {courses.map(course => (
@@ -64,7 +21,6 @@ const Courses = (props) => {
 }
 
 const Header = (props) => {
-  console.log(props)
   return (
     <div>
       <h1>{props.name}</h1>
@@ -74,7 +30,6 @@ const Header = (props) => {
 
 
 const Content = (props) => {
-  console.log(props)
   /*2.3*: kurssitiedot step8*/
   const total = props.parts.reduce((sum, part) => sum + part.exercises, 0);
 
@@ -89,7 +44,6 @@ const Content = (props) => {
 }
 
 const Part = (props) => {
-  console.log(props)
   return (
     <div>
       <p>{props.name} {props.exercises}</p>
@@ -99,7 +53,6 @@ const Part = (props) => {
 
 /*2.2: kurssitiedot step7*/
 const Total = (props) => {
-  console.log(props)
   return (
     <div>
       <p>Total sum of submitted exercises is {props.total}</p>
