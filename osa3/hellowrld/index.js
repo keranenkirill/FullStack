df8e773e95c3a388e4bd9d942d5e1f49b38be9e4
Kirill Keränen <keranen.kirill@gmail.com>
2026-08-16 18:46:43 +0300

    harjoitus upd

--- osa3/hellowrld/index.js
+++ osa3/hellowrld/index.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const app = express()
+
+// Middleware 1:
+// Parsii JSON-muotoisen request bodyn JavaScript-olioksi
 app.use(express.json())
 
+// Middleware 2:
+// Tulostaa jokaisen pyynnön perustiedot
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+
+  next()
+}
+
+app.use(requestLogger)
 
 let notes = [
   {
@@ -23,52 +38,29 @@ let notes = [
 
 const generateId = () => {
   const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
+    ? Math.max(...notes.map(note => Number(note.id)))
     : 0
+
   return String(maxId + 1)
 }
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
-
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    id: generateId(),
-  }
-
-  notes = notes.concat(note)
-
-  console.log(note)
-  response.json(note)
-  console.log(request.headers)
-})
+// ROUTET
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World! ^_^/ UWU</h1>')
 })
 
+
 app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
-})
 
 app.get('/api/notes/:id', (request, response) => {
   const id = request.params.id
+
   const note = notes.find(note => note.id === id)
-  
 
   if (note) {
     response.json(note)
@@ -77,7 +69,51 @@ app.get('/api/notes/:id', (request, response) => {
   }
 })
 
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId()
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+
+// Middleware 3:
+// Tämä suoritetaan vain, jos mikään yllä olevista routeista
+// ei ole käsitellyt pyyntöä
+const unknownEndpoint = (request, response) => {
+  response.status(404).json({
+    error: 'unknown endpoint'
+  })
+}
+
+app.use(unknownEndpoint)
+
+
 const PORT = 3001
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
